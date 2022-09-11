668f416ceb11d31ff930c1ab716424b95946e066
myfriendtania <thetalentedleo2017@gmail.com>
2022-09-11 16:45:31 -0500

    added a space

--- app.js
+++ app.js
@@ -6,5 +6,6 @@
 const modalBtn = document.querySelector(".modal-btn");
 const closeBtn = document.querySelector(".close-btn");
 
-modalBtn.addEventListener("click", function() {
+modalBtn.addEventListener("click", function () {
+
 });
