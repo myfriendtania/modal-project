3284eca50b5489d48c1d0da5d2f19995980b932b
myfriendtania <thetalentedleo2017@gmail.com>
2022-10-24 17:09:36 -0500

    completed Modal Project

--- app.js
+++ app.js
@@ -4,8 +4,13 @@
 // when user clicks close-btn remove .open-modal from modal-overlay
 
 const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
 
 modalBtn.addEventListener("click", function () {
+    modal.classList.add('open-modal')
+})
 
+closeBtn.addEventListener("click", function () {
+    modal.classList.remove('open-modal')
 });
